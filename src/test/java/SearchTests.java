aa470f470ff2f17d23f477da8a853961698033ba
Ilia <imerofeev@gmail.com>
2023-04-23 08:32:58 +0300

    new changes for ru region

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -9,6 +9,6 @@ public class SearchTests {
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.ru"));
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
 }
