432a42fce8e12bfcf8ff7929aacf780c80f2d188
Ilia Erofeev <imerofeev@gmail.com>
2023-04-24 08:41:00 +0300

    Update SearchTests.java
    fix

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -9,6 +9,6 @@ public class SearchTests {
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://us.selenide.org"));
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
 }
