4629148ab1e1f93ad4c0938c9d71b711bfa46554
Ilya Erofeev <ilya.erofeev@rtkit.ru>
2023-05-05 14:27:34 +0300

    add: basic logical operations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,5 +18,11 @@ public class Main {
         System.out.println("a < b = " + (a < b));
         System.out.println("b >= a = " + (b >= a));
         System.out.println("b <= a = " + (b <= a) + "\n");
+
+        boolean c = true;
+        boolean d = false;
+        System.out.println("Logical 'and': " + (c && d));
+        System.out.println("Logical 'or': " + (d || c));
+        System.out.println("Logical 'not': " + !c + "\n");
     }
 }
