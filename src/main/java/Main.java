d3bbb9ff37dc04ef050b1165d96cbd37dff8edd0
Ilya Erofeev <ilya.erofeev@rtkit.ru>
2023-05-05 14:18:50 +0300

    add: basic comparison operations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,13 @@ public class Main {
         System.out.println("a % b = " + (a % b));
         System.out.println("a++ = " + (a++));
         System.out.println("++a = " + (++a));
-        System.out.println("--b = " + (--b));
+        System.out.println("--b = " + (--b) + "\n");
+
+        System.out.println("a == b = " + (a == b));
+        System.out.println("a != b = " + (a != b));
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("b >= a = " + (b >= a));
+        System.out.println("b <= a = " + (b <= a) + "\n");
     }
 }
