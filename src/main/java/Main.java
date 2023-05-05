31a74c8ec2d28edb4c37632bd4be2be08b9374a8
Ilya Erofeev <ilya.erofeev@rtkit.ru>
2023-05-05 13:38:39 +0300

    add: basic arithmetic operations

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String args[]) {
+        int a = 20;
+        int b = 5;
+        System.out.println("a = " + a + "\n" + "b = " + b);
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("a++ = " + (a++));
+        System.out.println("++a = " + (++a));
+        System.out.println("--b = " + (--b));
+    }
+}
